5e9c32000c657adf39894367ac03cba6897d0ea4
Georgemuchiri <georgw.muchiri@student.moringaschool.com>
2024-09-09 12:22:23 +0300

    Add solution

--- lib/testing/user_test.py
+++ lib/testing/user_test.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
 from user import User
+# user.py
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
 
 class TestUser:
     '''Class "User" in user.py'''
@@ -11,5 +17,5 @@ class TestUser:
 
     def test_initializes_with_names(self):
         '''initializes with first and last name.'''
-        my_user = User("My", "User")
+        my_user =User("My", "User")
         assert((my_user.first_name, my_user.last_name) == ("My", "User"))
